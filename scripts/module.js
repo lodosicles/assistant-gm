bd3cf682d4d7c76bff9d06633e1b39bbf8649cc9
SpareBoot <burns.cameron@gmail.com>
2024-07-21 22:02:57 +1000

    Create module.js

---
+++ scripts/module.js
@@ -0,0 +1,28 @@
+// scripts/module.js
+
+import { AssistantGM } from './AssistantGM.js';
+
+Hooks.once('init', async function() {
+    console.log('AssistantGM | Initializing');
+    await AssistantGM.init();
+});
+
+Hooks.once('ready', async function() {
+    console.log('AssistantGM | Ready');
+    await AssistantGM.ready();
+});
+
+Hooks.on('renderSettingsConfig', (app, html, data) => {
+    const modelSetting = html.find(`select[name="assistant-gm.modelName"]`);
+    const fetchButton = $('<button type="button" id="fetch-models">Fetch Models</button>');
+    modelSetting.after(fetchButton);
+    fetchButton.click(async (event) => {
+        event.preventDefault();
+        await AssistantGM.updateAvailableModels(app);
+    });
+});
+
+// Expose the API
+Hooks.once('setup', () => {
+    game.modules.get('assistant-gm').api = AssistantGM;
+});
